8196026e5ad385d525d51adfc986703339f1d9aa
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2025-11-18 05:42:37 +0300

    notifications & arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,26 @@ import { clerkMiddleware } from "@clerk/express";
 // Routes
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
+<<<<<<< Updated upstream
+=======
+import commentRoute from "./routes/comment.route.js";
+import notificationRoute from "./routes/notification.route.js";
+>>>>>>> Stashed changes
 
 import { connectDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 const PORT = ENV.PORT;
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(clerkMiddleware());
+
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hello From SERVER");
@@ -25,6 +35,12 @@ app.use("/api/users", userRoute);
 
 // Post Routes
 app.use("/api/posts", postRoute);
+
+// Comment Routes
+app.use("/api/comment", commentRoute);
+
+// Notification Routes
+app.use("/api/notifications", notificationRoute);
 
 
 // Error Handling Middleware
