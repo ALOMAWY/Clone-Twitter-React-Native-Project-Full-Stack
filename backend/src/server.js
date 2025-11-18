3222fe1422e9e423ccbfebb94aa413ab3bfa2e05
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2025-11-18 06:00:09 +0300

    fix imports and un comment some comments

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,9 @@ import { clerkMiddleware } from "@clerk/express";
 // Routes
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
-<<<<<<< Updated upstream
-=======
 import commentRoute from "./routes/comment.route.js";
 import notificationRoute from "./routes/notification.route.js";
->>>>>>> Stashed changes
+
 
 import { connectDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
