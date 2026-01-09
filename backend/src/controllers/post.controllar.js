fbee4cf8e9963d8b3652575fc8b47d28c02c29d6
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2026-01-09 13:53:16 +0300

    fix populating the user name fields as cammelcase

--- backend/src/controllers/post.controllar.js
+++ backend/src/controllers/post.controllar.js
@@ -11,12 +11,12 @@ import cloudinary from "../config/cloudinary.js";
 export const getPosts = asyncHandler(async (req, res) => {
   const posts = await Post.find()
     .sort({ createdAt: -1 })
-    .populate("user", "username firstname lastname profilePicture")
+    .populate("user", "username firstName lastName profilePicture")
     .populate({
       path: "comments",
       populate: {
         path: "user",
-        select: "username firstname lastname profilePicture",
+        select: "username firstName lastName profilePicture",
       },
     });
 
