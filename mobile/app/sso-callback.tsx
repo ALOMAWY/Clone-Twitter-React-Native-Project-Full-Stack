5a27a1295e252d2f2375335ac0af1be9a47b594d
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2026-01-10 23:56:18 +0300

    fix redirect after authentecating

---
+++ mobile/app/sso-callback.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+import { View, ActivityIndicator } from "react-native";
+
+export default function SsoCallback() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.replace("/(tabs)");
+    }
+  }, [isSignedIn]);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+}
