a77d501bec12f76ce49701f85f5754e36b4b1b1f
Abdalrahman_Aldabbas <107769558+ALOMAWY@users.noreply.github.com>
2026-01-20 00:54:04 +0300

    Update notification.controllar.js

--- backend/src/controllers/notification.controllar.js
+++ backend/src/controllers/notification.controllar.js
@@ -8,13 +8,13 @@ export const getNotifications = asyncHandler(async (req, res) => {
   const user = await User.findOne({ clerkId: userId });
   if (!user) return res.status(404).json({ error: "User not found" });
 
-  const notification = await Notification.find({ to: user._id })
+  const notifications = await Notification.find({ to: user._id })
     .sort({ createdAt: -1 })
     .populate("from", "firstName lastName username profilePicture")
     .populate("post", "content image")
     .populate("comment", "content");
 
-  res.status(200).json({ notification });
+  res.status(200).json({ notifications });
 });
 
 export const deleteNotification = asyncHandler(async (req, res) => {
